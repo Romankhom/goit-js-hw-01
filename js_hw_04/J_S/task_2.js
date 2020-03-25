9fc95541be35a098ac746e395ffca4b8ea50424e
Romankhom <homenko.r.v@gmail.com>
2020-03-25 20:27:28 +0200

    module-2

--- js_hw_04/J_S/task_2.js
+++ js_hw_04/J_S/task_2.js
@@ -1,11 +1,11 @@
-"use strict";
+"use strict ";
 
 const inventory = {
-  items: ['Knife', 'Gas mask'],
+  items: ["Knife", "Gas mask"],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
-    inventory.items.push(itemName);
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
@@ -19,13 +19,13 @@ const invokeInventoryAction = function (itemName, action) {
   action(itemName);
 };
 
-invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
